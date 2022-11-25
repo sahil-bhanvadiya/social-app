13d57ce53e86568fb420c91e9c2262c2fc745a1c
sahil-bhanvadiya <sahil.bhanvadiya.dev@gmail.com>
2022-11-25 16:04:38 +0530

    feat: initial setup

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,9 +1,9 @@
 import { configureStore, ThunkAction, Action } from '@reduxjs/toolkit';
-import counterReducer from '../features/counter/counterSlice';
+import counterSlice from './slices/counterSlice';
 
 export const store = configureStore({
   reducer: {
-    counter: counterReducer,
+    counter: counterSlice,
   },
 });
 
---
+++ src/utils/Theme.ts
@@ -0,0 +1,13 @@
+import { createTheme } from '@mui/material/styles';
+
+export const Theme = createTheme({
+    palette: {
+        mode: 'light',
+        primary: {
+          main: '#5093a0',
+        },
+        secondary: {
+          main: '#f50057',
+        },
+      },
+  });
